4140e4e41c22d941fc12cb437b0677d05619a10f
Joel Ramirez <joer04011992@gmail.com>
2016-08-29 16:40:52 -0500

    Se añadio Cuentas por Cobrar

---
+++ Medicontrol/Facturacion/CuentasporCobrar.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Facturacion/Facturacion.Master" AutoEventWireup="true" CodeBehind="CuentasporCobrar.aspx.cs" Inherits="Medicontrol.Facturacion.WebForm4" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="http://ajax.aspnetcdn.com/ajax/jquery.ui/1.9.2/jquery-ui.min.js" type="text/javascript"></script>
+    <link href="http://ajax.aspnetcdn.com/ajax/jquery.ui/1.9.2/themes/blitzer/jquery-ui.css" rel="Stylesheet" type="text/css" />
+    <asp:ScriptManager EnablePartialRendering="true" ID="ScriptManager1" runat="server"></asp:ScriptManager>
+    <div class="alert alert-danger">
+        <asp:Label runat="server" ID="lbl_resultado" Text=""></asp:Label>
+    </div>
+    <div class="form-group">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-3">
+            <asp:Panel runat="server" ID="PanelBusqueda" DefaultButton="btn_buscar">
+                <asp:Label runat="server" ID="CodigoSesion" Visible="false" ></asp:Label>
+                <asp:Label runat="server" ID="lbl_entidad" Text="Numero de Factura"></asp:Label>
+                <asp:TextBox runat="server" ID="txt_numFactura" CssClass="form-control"></asp:TextBox>
+                <asp:Button runat="server" ID="btn_buscar" CssClass="btn btn-link" Text=" " OnClick="btn_buscar_Click" />
+                <br />
+            </asp:Panel>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-3">
+            <asp:Panel runat="server" ID="Panel1" DefaultButton="btn_buscarDocumento">
+
+                <asp:Label runat="server" ID="lbl_contrato" Enabled="false" Text="Documento de identidad"></asp:Label>
+                <asp:TextBox runat="server" ID="txt_Documento" CssClass="form-control"></asp:TextBox>
+                <asp:Button runat="server" ID="btn_buscarDocumento" CssClass="btn btn-link" Text=" " OnClick="btn_buscarDocumento_Click" />
+                <br />
+            </asp:Panel>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+            <asp:Label runat="server" ID="Label1" Enabled="false" Text="Nombres"></asp:Label>
+            <asp:TextBox runat="server" ID="txt_nombre" CssClass="form-control"></asp:TextBox>
+            <br />
+            <br />
+        </div>
+
+    </div>
+    <div class="form-group">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-3">
+            <asp:Label runat="server" ID="Label2" Enabled="false" Text="Telefono"></asp:Label>
+            <asp:TextBox runat="server" ID="txt_telefono" ReadOnly="true" CssClass="form-control"></asp:TextBox><br />
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-9">
+            <asp:Label runat="server" ID="Label3" Enabled="false" Text="Direccion de Residencia"></asp:Label>
+            <asp:TextBox runat="server" ID="txt_direccion" ReadOnly="true" CssClass="form-control"></asp:TextBox><br />
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-3">
+            <asp:Label runat="server" ID="Label4" Text="Fecha"></asp:Label>
+            <asp:TextBox runat="server" ID="txt_fecha" CssClass="form-control birthday"></asp:TextBox>
+            <br />
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-3">
+            <asp:Label runat="server" ID="Label5" Enabled="false" Text="Valor de la Factura"></asp:Label>
+            <asp:TextBox runat="server" ID="txt_valor" CssClass="form-control"></asp:TextBox>
+            <br />
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+            <asp:Label runat="server" ID="Label6" Enabled="false" Text="Detalle"></asp:Label>
+            <asp:TextBox runat="server" ID="txt_detalle" CssClass="form-control"></asp:TextBox>
+            <br />
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <asp:Button runat="server" ID="btn_guardar" Text="Guardar" CssClass="btn btn-primary" OnClick="btn_guardar_Click" />
+        </div>
+    </div>
+
+    <script src="../Scripts/bootstrapcantidad/bootstrap.min.js"></script>
+</asp:Content>
---
+++ Medicontrol/Facturacion/CuentasporCobrar.aspx.cs
@@ -0,0 +1,157 @@
+﻿using Helper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Medicontrol.Facturacion
+{
+    public partial class WebForm4 : System.Web.UI.Page
+    {
+        IngresarDatos Datos = new IngresarDatos();
+        private string ruta = ConfigurationManager.ConnectionStrings["System-conection"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Cookies["Login"] != null)
+            {
+                CodigoSesion.Text = Request.Cookies["Login"]["ID"].ToString();
+            }
+            else
+            {
+                Response.Redirect("../Login.aspx");
+            }
+        }
+
+        protected void btn_buscar_Click(object sender, EventArgs e)
+        {
+            if (txt_numFactura.Text == string.Empty)
+            {
+                lbl_resultado.Text = "Debe digitar un numero de factura";
+                return;
+            }
+            else
+            {
+                string consulta = "SELECT FacturaCab.NumFac AS NumFactura, FacturaCab.TipoDoc AS TipoDoc, FacturaCab.Prefijo AS Prefijo, Pacientes.Documento AS Documento, (Pacientes.Nombre1+' '+Pacientes.Nombre2+' '+Pacientes.Apellido1+' '+Pacientes.Apellido2) AS Nombre, Pacientes.Direccion AS Direccion, Pacientes.Telefono AS Telefono, FacturaCab.FechaFactura AS FechaFactura, FacturaCab.VrTotalEntidad AS ValorEntidad " +
+                                  "FROM Pacientes INNER JOIN FacturaCab ON Pacientes.Documento = FacturaCab.PDocumento Where FacturaCab.TipoDoc = '1' AND FacturaCab.NumFac = '" + this.txt_numFactura.Text + "'";
+                SqlConnection Conexion = new SqlConnection(ruta);
+                SqlCommand comando = new SqlCommand(consulta, Conexion);
+                Conexion.Open();
+                SqlDataReader leer = comando.ExecuteReader();
+                if (leer.Read() == true)
+                {
+                    lbl_resultado.Text = string.Empty;
+                    txt_Documento.Text = leer["Documento"].ToString();
+                    txt_nombre.Text = leer["Nombre"].ToString();
+                    txt_telefono.Text = leer["Telefono"].ToString();
+                    txt_direccion.Text = leer["Direccion"].ToString();
+                    DateTime fecha = Convert.ToDateTime(leer["FechaFactura"].ToString());
+                    txt_fecha.Text = fecha.ToString("dd/MM/yyyy");
+                    txt_valor.Text = leer["ValorEntidad"].ToString();
+                }
+                else
+                {
+                    lbl_resultado.Text = "El numero de factura no existe verifique";
+                    return;
+                }
+                Conexion.Close();
+            }
+        }
+
+        protected void btn_buscarDocumento_Click(object sender, EventArgs e)
+        {
+            if (txt_Documento.Text == string.Empty)
+            {
+                lbl_resultado.Text = "Debe digitar un documento para consultar";
+                return;
+            }
+            else
+            {
+                string consulta = "SELECT Documento, (Nombre1+' '+Nombre2+' '+Apellido1+' '+Apellido2) AS Nombre, Telefono, Direccion FROM Pacientes WHERE Documento='" + this.txt_Documento.Text + "'";
+                SqlConnection Conexion = new SqlConnection(ruta);
+                SqlCommand comando = new SqlCommand(consulta, Conexion);
+                Conexion.Open();
+                SqlDataReader leer = comando.ExecuteReader();
+                if (leer.Read() == true)
+                {
+                    lbl_resultado.Text = string.Empty;
+                    txt_nombre.Text = leer["Nombre"].ToString();
+                    txt_telefono.Text = leer["Telefono"].ToString();
+                    txt_direccion.Text = leer["Direccion"].ToString();
+                }
+                else
+                {
+                    lbl_resultado.Text = "No existe ningun paciente con ese Documento";
+                    return;
+                }
+                Conexion.Close();
+            }
+        }
+
+        protected void btn_guardar_Click(object sender, EventArgs e)
+        {
+            if(txt_numFactura.Text==string.Empty)
+            {
+                lbl_resultado.Text = "Falta el numero de la factura";
+                return;
+            }
+            if (txt_Documento.Text == string.Empty)
+            {
+                lbl_resultado.Text = "falta el documento del paciente";
+                return;
+            }
+            if (txt_fecha.Text==string.Empty)
+            {
+                lbl_resultado.Text = "Falta la fecha de la factura";
+                return;
+            }
+            if(txt_valor.Text==string.Empty)
+            {
+                lbl_resultado.Text = "Falta el valor de la factura";
+                return;
+            }
+
+            if (!Utilidades.isNumeric(txt_valor.Text))
+            {
+                lbl_resultado.Text = "Digite una cantidad correcta";
+                return;
+            }
+            if (Convert.ToDouble(txt_valor.Text) < 0)
+            {
+                lbl_resultado.Text = "Digite una cantidad correcta";
+                return;
+            }
+            NumberStyles style;
+            CultureInfo culture;
+            double number;
+            style = NumberStyles.Number | NumberStyles.AllowCurrencySymbol;
+            string value = txt_valor.Text;
+            culture = CultureInfo.CreateSpecificCulture("en-US");
+            if (Double.TryParse(value, style, culture, out number))
+            {
+                txt_valor.Text = number.ToString();
+            }
+            else
+            {
+                lbl_resultado.Text = "Digite una cantidad correcta";
+                return;
+            }
+
+            
+            DateTime fecha = Convert.ToDateTime(ViewHelper.ConvertToDate(txt_fecha.Text));
+            string query = "INSERT INTO CuentasporCobrar(NumFactura, CodigoCliente, Detalle, Fecha, ValorFactura, ValorAbonos, ValorSaldo, CodigoVendedor) VALUES('" + this.txt_numFactura.Text + "', '" + this.txt_Documento.Text + "', '" + this.txt_detalle.Text + "', '" + fecha + "', '" + this.txt_valor.Text + "', '0', '" + this.txt_valor.Text + "', '" + this.CodigoSesion.Text + "')";
+            if (Datos.insertar(query))
+            {
+                lbl_resultado.Text = "Error al guardar, Verifique";
+            }
+            else
+            {
+                lbl_resultado.Text = "Registro Guardado correctamente";
+            }
+        }
+    }
+}
